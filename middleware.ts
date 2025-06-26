e25ad4d050db018d5aecb92a81e4478e9aad4c89
Asmita pawar <asmitapawar506@gmail.com>
2025-06-26 16:47:18 +0530

    resolved merge conflicts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
-// middleware.ts
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware();
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip static files and Next.js internals
-    '/((?!_next|.*\\..*|favicon.ico).*)',
-    // Include API and tRPC routes
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
